e215d549dcdf447eed8cf4fdc7d3eb5bbf64ca81
flaviogardim <flaviogardim@edu.lucasdorioverde.mt.gov.br>
2026-07-15 21:41:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbyTYr2hI0JL4ZSDSn6jZOT5zDnCNHjCP6tWoPPrdnWCWYfZ5iquCopPuUbp6FqYKlM/exec";
 
-async function testarAPI() {
+window.onload = async function () {
 
-    const dados = {
+    try {
 
-        nome: "Teste",
+        const resposta = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                nome: "Teste GitHub",
+                data: "2026-07-15",
+                horario1: "08:00",
+                horario2: "",
+                justificativa: "Primeiro teste"
+            })
+        });
 
-        data: "2026-07-16",
+        const resultado = await resposta.json();
 
-        horario1: "08:00",
+        alert(JSON.stringify(resultado));
 
-        horario2: "",
+    } catch (e) {
 
-        justificativa: "Teste de comunicação"
+        alert(e.message);
 
-    };
+    }
 
-    const resposta = await fetch(API_URL, {
-
-        method: "POST",
-
-        headers: {
-            "Content-Type": "application/json"
-        },
-
-        body: JSON.stringify(dados)
-
-    });
-
-    const retorno = await resposta.json();
-
-    console.log(retorno);
-
-    alert(JSON.stringify(retorno));
-
-}
-
-window.onload = testarAPI;
+};
